8637daead3e12b23a15e4ee2d999a7a505e813e0
MilkTeaD <milktea.original@gmail.com>
2022-04-29 12:51:40 +0200

    commit met foute test

--- src/main/java/be/vdab/goededoel/GoedeDoelApplication.java
+++ src/main/java/be/vdab/goededoel/GoedeDoelApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import java.math.BigDecimal;
+
 @SpringBootApplication
 public class GoedeDoelApplication extends SpringBootServletInitializer {
-
+	private BigDecimal opbrengst = BigDecimal.ZERO;
 	public static void main(String[] args) {
 		SpringApplication.run(GoedeDoelApplication.class, args);
 	}
